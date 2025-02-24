b1e395295128a23c2c9240108c0a5006a76dbef9
atarusov <tvinkvinter@gmail.com>
2025-02-24 18:50:18 +0300

    refactor: remove redundant empty lines @SerializedName annotations

--- app/src/main/java/com/atarusov/musicplayer/features/apitracks/data/model/ChartItem.kt
+++ app/src/main/java/com/atarusov/musicplayer/features/apitracks/data/model/ChartItem.kt
@@ -1,18 +1,17 @@
 package com.atarusov.musicplayer.features.apitracks.data.model
 
 import com.atarusov.musicplayer.features.apitracks.domain.model.Track
-import com.google.gson.annotations.SerializedName
 
 data class ChartItem(
     val id: Long,
-    @SerializedName("title") val trackTitle: String?,
+    val title: String?,
     val album: Album,
     val artist: Artist,
 ) {
     fun toTrack(): Track = Track(
         id = id,
         coverURL = album.coverURL,
-        trackTitle = trackTitle,
+        trackTitle = title,
         artistName = artist.name,
     )
 }
